dd16c7744dee60363d5b2ee6ab804d0f28d47a3d
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2022-01-21 10:25:58 +0100

    Inital commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0b512129255bba028f647cc179154943'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '859a7f2b-f790-4be9-88bb-ccde6e22c8b9'
+};
